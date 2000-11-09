076d6250b04c5c1349db46117976d482d6df850e
Matthew Emmerton <matt@gsicomp.on.ca>
2000-11-09 17:55:31 +0000

    Somehow the 'local include' file missed my commit

---
+++ src/btree/btreei.h
@@ -0,0 +1,28 @@
+#ifndef _BTREE_PRIV_H
+#define _BTREE_PRIV_H
+
+#include "btree.h"
+
+/* addkey.c */ 
+void addkey(BTREE, FKEY, RKEY, FKEY);
+
+/* block.c */
+BLOCK crtblock(BTREE);
+BLOCK allocblock(void);
+
+/* index.c */
+INDEX crtindex(BTREE);
+INDEX getindex(BTREE, FKEY);
+void initcache(BTREE, INT);
+void putheader(BTREE, BLOCK);
+void putindex(BTREE, INDEX);
+INDEX readindex(STRING, FKEY);
+void writeindex(STRING, INDEX);
+
+/* utils.c */
+BOOLEAN newmaster(BTREE, INDEX);
+FKEY path2fkey(STRING);
+void nextfkey(BTREE);
+BOOLEAN mkalldirs(char *);
+
+#endif /* _BTREE_PRIV_H */
