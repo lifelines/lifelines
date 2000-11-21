bcfc15fe5ed1d474e663772338f646f5e79ca0e5
Matthew Emmerton <matt@gsicomp.on.ca>
2000-11-21 06:00:57 +0000

    Added do-nothing stub routine to arch/ (and updated configure to search for that routine) to force libarch.a to always have at least one object in it.  (This avoids problems with 'ar' on OpenBSD.)

---
+++ src/arch/_llnull.c
@@ -0,0 +1,13 @@
+/* _llnull.c */
+
+/*
+This is a do-nothing routine which will force autoconf/automake to
+nclude this object into libarch.a.  This allows OSes that cannot
+handle a 'ar cru libarch.a' without any objects being specified
+to build this library, and later on link with it.
+*/
+
+void _llnull(void)
+{
+  return;
+}
