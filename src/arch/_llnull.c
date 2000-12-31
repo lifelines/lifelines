fd95d056afc3769e58ab65630b2610473eee3555
Petter Reinholdtsen <pere@users.sourceforge.net>
2000-12-31 01:23:30 +0000

    Make dummy function static to avoid exporting unused symbol _llnull. Fix typo in comment.

--- src/arch/_llnull.c
+++ src/arch/_llnull.c
@@ -2,12 +2,13 @@
 
 /*
 This is a do-nothing routine which will force autoconf/automake to
-nclude this object into libarch.a.  This allows OSes that cannot
+include this object into libarch.a.  This allows OSes that cannot
 handle a 'ar cru libarch.a' without any objects being specified
 to build this library, and later on link with it.
 */
 
-void _llnull(void)
+static void _llnull(void)
 {
+  _llnull(); /* Avoid 'warning: `_llnull' defined but not used' */
   return;
 }
