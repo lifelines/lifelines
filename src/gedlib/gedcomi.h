f7d033a93cd66c5e8838f470db7773ebd7a6948d
Matthew Emmerton <matt@gsicomp.on.ca>
2001-01-02 02:54:10 +0000

    Add #ifdef wrappers

--- src/gedlib/gedcomi.h
+++ src/gedlib/gedcomi.h
@@ -1,5 +1,10 @@
+#ifndef _GEDCOM_PRIV_H
+#define _GEDCOM_PRIV_H
+
 /* from charmaps.c */
 TRANTABLE init_map_from_file(STRING, INT, BOOLEAN*);
 
 /* from names.c */
 STRING* id_by_key(STRING, STRING**);
+
+#endif /* _GEDCOM_PRIV_H */
--- src/stdlib/stdlibi.h
+++ src/stdlib/stdlibi.h
@@ -1,1 +1,6 @@
+#ifndef _STDLIB_PRIV_H
+#define _STDLIB_PRIV_H
+
 void ll_abort(int);
+
+#endif /* _STDLIB_PRIV_H */
