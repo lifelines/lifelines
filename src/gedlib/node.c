8e9508e64ade1aea9dfd133980b4106a22137804
Matthew Emmerton <matt@gsicomp.on.ca>
2003-02-03 00:56:14 +0000

    Back out my fix temporarily (with a comment block)

--- src/gedlib/node.c
+++ src/gedlib/node.c
@@ -173,10 +173,15 @@ free_node (NODE node)
 {
 	if (nxref(node)) stdfree(nxref(node));
 	if (nval(node)) stdfree(nval(node));
+/* MTE 2003/02/02
+ * Is this the right thing to do?
+ * Or do tags have to live longer than nodes?
+
 	if (ntag(node)) {
           delete_table(tagtable,ntag(node));
           stdfree(ntag(node));
         }
+*/
 	((NDALLOC) node)->next = first_blck;
 	first_blck = (NDALLOC) node;
 	
