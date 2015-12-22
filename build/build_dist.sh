c59a26e1cc18e4b2e3a3bfe7e8ca8354e5d6b55d
Matt Emmerton <matt@gsicomp.on.ca>
2015-12-22 16:56:02 -0500

    Update release script

--- build/build_dist.sh
+++ build/build_dist.sh
@@ -1,10 +1,28 @@
 #!/bin/sh
 
-if [ ! -e "build_dist.sh" ]
+# Determine root of repository
+if [ ! -f build_dist.sh ]
 then
- echo 'ERROR: This script must be run from the build/ directory.'
- exit 1
+  if [ ! -f ../build_dist.sh ]
+  then
+    echo "ERROR: Must be run from either the root of the source tree or the build/ directory!"
+    exit 1
+  else
+    ROOTDIR=..
+  fi
+else
+  ROOTDIR=.
 fi
+
+UNAME=`uname`
+if [ $UNAME != "Linux" ]
+then
+  echo "ERROR: Release tools only work on Linux due to bash-isms."
+  exit 1
+fi
+
+SAVEDIR=`pwd`
+cd $ROOTDIR
 
 read -p 'Press key to fetch latest code from git:'
 
@@ -12,10 +30,9 @@ echo "STATUS: Updating local git repository..."
 git checkout master
 git pull
 
-read -p 'Enter new version number: ' newversion
+read -p 'Enter new version number (format X.Y.Z): ' newversion
 
 echo ${newversion} | grep -E '^[[:digit:]]{1,2}\.[[:digit:]]{1,2}\.[[:digit:]]{1,2}$' > /dev/null
-
 if [ $? -eq 0 ]
 then
   continue 
@@ -25,12 +42,12 @@ else
 fi
 
 echo "STATUS: Changing version number to $newversion..."
-sh setversions.sh $newversion
+$ROOTDIR/build/setversions.sh $newversion
 
 read -p 'Press key to run autotools:'
 
 echo "STATUS: Generating new makefiles and configure script..."
-sh autogen.sh
+$ROOTDIR/build/autogen.sh
 
 read -p 'Press key to recreate local build subdirectory:'
 
@@ -38,7 +55,7 @@ echo "STATUS: Creating local build subdirectory..."
 rm -rf bld
 mkdir bld
 
-read -p 'Press key to run configure:
+read -p 'Press key to run configure:'
 
 echo "STATUS: Running ./configure..."
 cd bld
@@ -50,6 +67,14 @@ echo "STATUS: Building..."
 cd ..
 make
 
+read -p 'Press key to build new language files:'
+
+echo "STATUS: Building language files..."
+mv po/lifelines.pot po/lifelines.pot.old
+cd po
+make lifelines.pot
+cd ..
+
 read -p 'Press key to build distribution tarball:'
 
 echo "STATUS: Creating distribution tarball..."
@@ -58,4 +83,10 @@ cd ..
 
 echo "STATUS: Almost done..."
 echo "You must read docs/dev/README.MAINTAINERS and finish the rest of the process!"
+echo "Things to do include:"
+echo "- tagging repo at github"
+echo "- creating a release at github and uploading release packages"
+echo "- sending message files to the translation project and updating message catalogs"
+echo "- testing"
 
+cd $SAVEDIR
