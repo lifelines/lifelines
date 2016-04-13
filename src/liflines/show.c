e99eb7778f55f4f15f6b63c6df2b7680fbdc53c3
Matt Emmerton <matt@gsicomp.on.ca>
2016-04-12 20:14:40 -0400

    Resolve Git #308: INDI with BIRT/DEAT without DATE/PLAC displays Y

--- src/liflines/show.c
+++ src/liflines/show.c
@@ -254,7 +254,10 @@ disp_person_birthdeath (ZSTR zstr, RECORD irec, struct tag_prefix * tags, RFMT r
 			zs_apps(ztemp, place);
 		}
 		if (!date && !place) {
-			zs_apps(ztemp, "Y");
+                        // Git #308: INDI with BIRT/DEAT without DATE/PLAC displays "Y"
+                        // The 3.0.62 behaviour was to display nothing.
+                        // This sounds more appropriate so reverting to that behaviour.
+			//zs_apps(ztemp, "Y");
 		}
 		if (ct>1) {
 			zs_appf(ztemp, " (%d alt)", ct-1);
