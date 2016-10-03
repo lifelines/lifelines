0417f55becca9878a43d2afccfec7c74c19d4dfd
Matt Emmerton <matt@gsicomp.on.ca>
2016-10-03 12:49:16 -0400

    initial restructuring

--- src/hdrs/mycurses.h
+++ src/hdrs/mycurses.h
@@ -4,24 +4,21 @@
 
 #include "config.h"
 
-#if defined(HAVE_LIBNCURSESW)
-    #if defined(HAVE_NCURSESW_CURSES_H)
-    #include <ncursesw/curses.h>
-    #elif defined(HAVE_NCURSES_CURSES_H)
-    #include <ncurses/curses.h>
-    #else
-    #include <curses.h>
-    #endif
-#elif defined(HAVE_LIBNCURSES)
-    #if defined(HAVE_NCURSES_CURSES_H)
-    #include <ncurses/curses.h>
-    #else
-    #include <curses.h>
-    #endif
-#elif defined(HAVE_LIBCURSES)
-#include <curses.h>
+/*
+ * Note that we do not use the HAVE_LIB*CURSES* #defines here, since the
+ * presence of libraries does not imply the presence of headers. This
+ * is because headers and libraries are packaged separately on many
+ * distros.
+ */
+
+#if defined(HAVE_NCURSESW_CURSES_H)
+  #include <ncursesw/curses.h>
+#elif defined(HAVE_NCURSES_CURSES_H)
+  #include <ncurses/curses.h>
+#elif defined(HAVE_CURSES_H)
+  #include <curses.h>
 #elif defined(HAVE_WINDOWS_H)
-#include "mswin/curses.h"
+    #include "mswin/curses.h"
 #else
-#error Unsupported curses configuration!
+    #error Unsupported curses configuration!
 #endif
