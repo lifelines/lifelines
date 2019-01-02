5e8a9f8b138d66018844366bfdc497b486f09a14
Matt Emmerton <matt@gsicomp.on.ca>
2019-01-02 11:36:17 -0500

    Use printf/scanf format macros in src/liflines and src/ui

--- src/liflines/main.c
+++ src/liflines/main.c
@@ -185,23 +185,23 @@ main (int argc, char **argv)
 					*optarg = tolower((uchar)*optarg);
 				if(*optarg == 'i') {
 					INT icsz_indi=0;
-					sscanf(optarg+1, "%ld,%ld", &csz_indi, &icsz_indi);
+					sscanf(optarg+1, SCN_INT "," SCN_INT, &csz_indi, &icsz_indi);
 				}
 				else if(*optarg == 'f') {
 					INT icsz_fam=0;
-					sscanf(optarg+1, "%ld,%ld", &csz_fam, &icsz_fam);
+					sscanf(optarg+1, SCN_INT "," SCN_INT, &csz_fam, &icsz_fam);
 				}
 				else if(*optarg == 's') {
 					INT icsz_sour=0;
-					sscanf(optarg+1, "%ld,%ld", &csz_sour, &icsz_sour);
+					sscanf(optarg+1, SCN_INT "," SCN_INT, &csz_sour, &icsz_sour);
 				}
 				else if(*optarg == 'e') {
 					INT icsz_even=0;
-					sscanf(optarg+1, "%ld,%ld", &csz_even, &icsz_even);
+					sscanf(optarg+1, SCN_INT "," SCN_INT, &csz_even, &icsz_even);
 				}
 				else if((*optarg == 'o') || (*optarg == 'x')) {
 					INT icsz_othr=0;
-					sscanf(optarg+1, "%ld,%ld", &csz_othr, &icsz_othr);
+					sscanf(optarg+1, SCN_INT "," SCN_INT, &csz_othr, &icsz_othr);
 				}
 				optarg++;
 				while(*optarg && isdigit((uchar)*optarg)) optarg++;
@@ -259,7 +259,7 @@ main (int argc, char **argv)
 			prog_trace = TRUE;
 			break;
 		case 'u': /* specify screen dimensions */
-			sscanf(optarg, "%ld,%ld", &winx, &winy);
+			sscanf(optarg, SCN_INT "," SCN_INT, &winx, &winy);
 			break;
 		case 'x': /* execute program */
 			if (!exprogs) {
