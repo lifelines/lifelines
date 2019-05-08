51651b4387bf462e34cf0f8404ee17a3bf6d361d
Matt Emmerton <matt@gsicomp.on.ca>
2019-05-07 21:00:07 -0400

    Support branches in sonarcloud
    Conflicts:
    	build/sonar.sh

--- build/sonar.sh
+++ build/sonar.sh
@@ -19,11 +19,9 @@ unzip build-wrapper-linux-x86.zip
 # Build with build wrapper
 (cd ..; build/build-wrapper-linux-x86/build-wrapper-linux-x86-64 --out-dir build/sonar_build_wrapper_output make clean all)
 
-# Run analysis
-# NOTE: This assumes that:
-# - the sonarqube server is running on the default port
-# - the sonar-scanner is installed and in $PATH
-(cd ..; sonar-scanner -Dproject.settings=build/sonar-project.properties -Dsonar.cfamily.build-wrapper-output=build/sonar_build_wrapper_output)
+# Run analysis (on current branch, assuming that the target branch will be master)
+MYBRANCH=`git status | head -1 | cut -f4 -d' '`
+(cd ..; echo sonar-scanner -Dproject.settings=build/sonar-project.properties -Dsonar.branch.target=master -Dsonar.branch.name=${MYBRANCH})
 
 # Remove build wrapper and scanner files
 rm -rf sonar_build_wrapper_output
