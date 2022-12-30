1b9f90eb012d263527421055d9fa8f364ba9ea9b
Matt Emmerton <matt@gsicomp.on.ca>
2022-12-30 01:36:01 -0500

    Ensure that ND_TEMP is set/cleared on entire tree

--- src/gedlib/node.c
+++ src/gedlib/node.c
@@ -259,14 +259,22 @@ create_temp_node (STRING xref, STRING tag, STRING val, NODE prnt)
 void
 free_temp_node_tree (NODE node)
 {
+	// It is an error to delete a node tree if it still has a parent.
+	ASSERT(nparent(node) == NULL);
+
+	// It is an error to delete a node that has a positive refcnt
+	ASSERT(nrefcnt(node) == 0);
+
 	NODE n2;
 	if ((n2 = nchild(node))) {
+		nparent(n2) = NULL;
 		free_temp_node_tree(n2);
-		nchild(node) = 0;
+		nchild(node) = NULL;
 	}
 	if ((n2 = nsibling(node))) {
+		nparent(n2) = NULL;
 		free_temp_node_tree(n2);
-		nsibling(node) = 0;
+		nsibling(node) = NULL;
 	}
 	free_node(node,"free_temp_node_tree");
 }
@@ -280,14 +288,69 @@ is_temp_node (NODE node)
 	return !!(nflag(node) & ND_TEMP);
 }
 /*===================================
+ * validate_temp_node_tree -- validate that node tree has consistent temp marking
+ * Created: 2022-12-30 (Matt Emmerton)
+ *=================================*/
+static BOOLEAN
+validate_temp_node_tree (NODE node, BOOLEAN temp)
+{
+	// All siblings / children of this node must be temp (or not).
+	// The parent can be different so it is not checked.
+	BOOLEAN valid = true;
+	NODE n2;
+	if ((n2 = nchild(node))) {
+		if (temp != is_temp_node(n2)) {
+			return false;
+		}
+		valid = validate_temp_node_tree(n2,temp);
+	}
+	if ((n2 = nsibling(node))) {
+		if (temp != is_temp_node(n2)) {
+			return false;
+		}
+		valid = validate_temp_node_tree(n2,temp);
+	}
+	return valid;
+}
+/*===================================
  * set_temp_node -- make node temp (or not)
  * Created: 2003-02-04 (Perry Rapp)
  *=================================*/
 void
 set_temp_node (NODE node, BOOLEAN temp)
 {
-	if (is_temp_node(node) ^ temp)
+	// Set this node as requested
+	if (is_temp_node(node) ^ temp) {
 		nflag(node) ^= ND_TEMP;
+	}
+
+	// Validate that all sibling and child nodes are the same
+	ASSERT(validate_temp_node_tree(node,temp);
+}
+/*===================================
+ * validate_temp_node_tree -- validate that node tree has consistent temp marking
+ * Created: 2022-12-30 (Matt Emmerton)
+ *=================================*/
+static BOOLEAN
+validate_temp_node_tree (NODE node, BOOLEAN temp)
+{
+	// All siblings / children of this node must be temp (or not).
+	// The parent can be different so it is not checked.
+	BOOLEAN valid = true;
+	NODE n2;
+	if ((n2 = nchild(node))) {
+		if (temp != is_temp_node(n2)) {
+			return false;
+		}
+		valid = validate_temp_node_tree(n2,temp);
+	}
+	if ((n2 = nsibling(node))) {
+		if (temp != is_temp_node(n2)) {
+			return false;
+		}
+		valid = validate_temp_node_tree(n2,temp);
+	}
+	return valid;
 }
 /*=====================================
  * free_nodes -- Free all NODEs in tree
@@ -295,13 +358,24 @@ set_temp_node (NODE node, BOOLEAN temp)
 void
 free_nodes (NODE node)
 {
-	NODE sib;
-	while (node) {
-		if (nchild(node)) free_nodes(nchild(node));
-		sib = nsibling(node);
-		free_node(node,"free_nodes");
-		node = sib;
+	// It is an error to delete a node tree if it still has a parent.
+	ASSERT(nparent(node) == NULL);
+
+	// It is an error to delete a node that has a positive refcnt
+	ASSERT(nrefcnt(node) == 0);
+
+	NODE n2;
+	if ((n2 = nchild(node))) {
+		nparent(n2) = NULL;
+		free_nodes(n2);
+		nchild(node) = NULL;
 	}
+	if ((n2 = nsibling(node))) {
+		nparent(n2) = NULL;
+		free_nodes(n2);
+		nsibling(node) = NULL;
+	}
+	free_node(node,"free_nodes");
 }
 /*==============================================================
  * tree_strlen -- Compute string length of tree -- don't count 0
